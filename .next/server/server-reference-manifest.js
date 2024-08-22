453bf0eba07396b6e8755165c07495c25d2042db
soj-i <sojioduneye0976@gmail.com>
2024-08-22 16:38:17 -0500

    Fix image optimization for static export

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"LJCquRqjs1qucPrPleqDk7QvX+RldF3PTYX0sBpb06w=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9ADeXfg+AOLppApJ1S3j9OiLiggIpBWeAjaPCcgx9xc=\"}"
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'out', // This is where the static files (including index.html) will be generated
+    images: {
+      unoptimized: true,
+    },
   };
   
   module.exports = nextConfig;
